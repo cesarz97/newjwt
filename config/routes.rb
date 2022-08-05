11af31ed93fc3f994e42b0cc33e6d4739e7e3239
lookout5mahesh@gmail.com <lookout5mahesh@gmail.com>
2022-08-05 12:24:12 +0530

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :books
  resource :users, only: [:create]
  post "/login", to: "users#login"
+ root 'hello#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_02_140924) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_books_on_user_id"
